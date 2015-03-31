27c185d85e9498b0485a6c7d90c97b7ba1c6eb15
Rishi Ramraj <thereisnocowlevel@gmail.com>
2015-03-31 14:52:14 -0400

    Fixing import error in dcp.apps.copy.

--- dcp/apps/copy.py
+++ dcp/apps/copy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from dcp.utils import options, utils, config
+from dcp.utils import options, misc, config
 from dcp.utils.exceptions import NoDatabase, BadConfig, InvalidTargets
 
 
@@ -13,8 +13,8 @@ def main():
     args = options.parser().parse_args()
 
     # Set the log level.
-    utils.set_log_level(args.log_level)
+    misc.set_log_level(args.log_level)
 
     # Parse the configuration.
-    with utils.catch(NoDatabase, BadConfig, InvalidTargets):
+    with misc.catch(NoDatabase, BadConfig, InvalidTargets):
         config.parse(args.source, args.destination)
